fb01e247a311fe35066a74d43093786fa41fd360
kjin41 <51227769+kjin41@users.noreply.github.com>
2021-11-10 00:52:02 +0900

    vuejs 코로나보건소 목록 - router

---
+++ vue_hw4-2/app.js
@@ -0,0 +1,21 @@
+import list from "./views/list.js";
+import detail from "./views/detail.js";
+
+const router=new VueRouter({
+  mode: 'history',
+  routes:[
+    {
+      path: '/',
+      component: list,
+    },
+    {
+      path: '/detail/:id',
+      component: detail,
+    },
+  ]
+});
+
+const app=new Vue({
+  el: "#app",
+  router,
+});
---
+++ vue_hw4-2/views/detail.js
@@ -0,0 +1,50 @@
+export default{
+  template: `
+  <div>
+  <h2>코로나 19 예방접종센터 상세 페이지</h2>
+  <table border="1">
+    <tbody>
+      <tr>
+        <td align="center">센터명</td>
+        <td>{{center.centerName}}</td>
+      </tr>
+      <tr>
+        <td align="center">센터유형</td>
+        <td>{{center.centerType}}</td>
+      </tr>
+      <tr>
+        <td align="center">주소</td>
+        <td>{{center.address}}</td>
+      </tr>
+      <tr>
+        <td align="center">센터시설명</td>
+        <td>{{center.org}}</td>
+      </tr>
+      <tr>
+        <td align="center">전화번호</td>
+        <td>{{center.phoneNumber}}</td>
+      </tr>
+      <tr>
+        <td align="center">생성일</td>
+        <td>{{center.createdAt}}</td>
+      </tr>
+      <tr>
+        <td align="center">수정일</td>
+        <td>{{center.updatedAt}}</td>
+      </tr>
+    </tbody>
+  </table>
+  </div>
+  `,
+  created(){
+    let id=this.$route.params.id;
+    this.center=this.centerList.data[id];
+    console.log(this.centerList.data[id]);
+  },
+  data(){
+    return{
+      center:{},
+      centerList
+    }
+  }
+}
